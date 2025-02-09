cbbe161564ad490c5755c31bfebeb892bd5b0848
Darrion Whitfield <dwhitfield@ramfield.net>
2025-02-09 01:33:06 -0600

    library changes

--- src/application/config.rs
+++ src/application/config.rs
@@ -16,26 +16,6 @@ pub fn get_config() -> AppConfig {
             std::process::exit(0)
         }
     };
-
-    let raw_version: SoftwareVersion = {
-        // defining the version
-        let library_version: Version = aml_version();
-        let software_version: Version = str_to_version(env!("CARGO_PKG_VERSION"), Some(VersionCode::Production));
-        
-        SoftwareVersion {
-            application: software_version,
-            library: library_version,
-        }
-    };
-
-    config.version = match serde_json::to_string(&raw_version) {
-        Ok(ver) => ver,
-        Err(err) => {
-            log!(LogLevel::Error, "{}", err);
-            std::process::exit(100);
-        },
-    };
-
     config.app_name = Stringy::from(env!("CARGO_PKG_NAME"));
     config.database = None;
     config
